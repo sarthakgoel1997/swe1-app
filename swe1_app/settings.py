7cf97c9602163f84981cd8cd8e7b9c1512598487
Sarthak Goel <sarthakgoel@Sarthaks-MBP.lan>
2024-02-15 11:52:32 -0500

    update django version

--- swe1_app/settings.py
+++ swe1_app/settings.py
@@ -78,10 +78,6 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': '',
-        'PORT': '',
     }
 }
 
